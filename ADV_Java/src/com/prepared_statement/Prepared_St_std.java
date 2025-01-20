f8fe35a78ed9df940e9d892ebbc444f84c3f2b06
vivekmengade <vivekmengade@gmail.com>
2025-01-20 20:16:29 +0530

    learned about Pre-Pared Statement

---
+++ ADV_Java/src/com/prepared_statement/Prepared_St_std.java
@@ -0,0 +1,29 @@
+package com.prepared_statement;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.Scanner;
+
+public class Prepared_St_std {
+	public static void main(String[] args) throws ClassNotFoundException, SQLException {
+		Class.forName("com.mysql.cj.jdbc.Driver");
+		Connection con= DriverManager.getConnection("jdbc:mysql://localhost:3306/studentJDBC","root","Vivek@123");
+		PreparedStatement ps= con.prepareStatement("select * from student where id=?");
+		Scanner sc = new Scanner(System.in);
+		int id = sc.nextInt();
+		ps.setInt(1, id);
+//		ResultSet rs = ps.getResultSet();
+		ResultSet rs = ps.executeQuery();
+		while(rs.next()) {
+			System.out.println(rs.getInt(1));
+			System.out.println(rs.getString(2));
+			System.out.println(rs.getDouble(3));
+		}
+		ps.close();
+		con.close();
+		
+	}
+}
---
+++ ADV_Java/src/com/prepared_statement/package-info.java
@@ -0,0 +1,1 @@
+package com.prepared_statement;
