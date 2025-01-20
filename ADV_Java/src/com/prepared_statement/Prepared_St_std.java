f7e2e875e8cb948b7c4e1e59db85a108bf52bf68
vivekmengade <vivekmengade@gmail.com>
2025-01-20 20:42:23 +0530

    done the scanner double use in code.

--- ADV_Java/src/com/prepared_statement/Prepared_St_std.java
+++ ADV_Java/src/com/prepared_statement/Prepared_St_std.java
@@ -11,20 +11,29 @@ public class Prepared_St_std {
 	public static void main(String[] args) throws ClassNotFoundException, SQLException {
 		Class.forName("com.mysql.cj.jdbc.Driver");
 		Connection con= DriverManager.getConnection("jdbc:mysql://localhost:3306/studentJDBC","root","Vivek@123");
-		PreparedStatement ps= con.prepareStatement("select * from student where id=?");
+		PreparedStatement ps = con.prepareStatement("update student set name=? where id = ?");
 		Scanner sc = new Scanner(System.in);
+		System.out.println("enter name updated of student");
+		String name = sc.nextLine();
+		System.out.println("enter id of student");
 		int id = sc.nextInt();
-		ps.setInt(1, id);
+		ps.setString(1, name);
+		ps.setInt(2, id);
+		int rwupdate = ps.executeUpdate();
+		System.out.println(rwupdate);
+		PreparedStatement ps2= con.prepareStatement("select * from student where id=?");
+		ps2.setInt(1,id);
 //		ResultSet rs = ps.getResultSet();
-//		ResultSet rs = ps.executeQuery();
-		ResultSet rs = ps.executeQuery();
+		ResultSet rs = ps2.executeQuery();
+//		ResultSet rs = ps.executeUpdate();
 		while(rs.next()) {
 			System.out.println(rs.getInt(1));
 			System.out.println(rs.getString(2));
 			System.out.println(rs.getDouble(3));
 		}
 		ps.close();
+		ps2.close();
 		con.close();
-		
+		sc.close();
 	}
 }
